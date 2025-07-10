c3e9468c1d2f68bb3ce8a5ab9b4f8321caddba61
Astaroth303 <marcofalchi68gmail.com>
2025-07-10 08:53:19 -0400

    modifiche

--- UNIT1/S2/L4/S2L4.py
+++ UNIT1/S2/L4/S2L4.py
@@ -4,26 +4,26 @@ import math #spiego successivamente perche ho importato la libreria math, ci ser
 def perimetro():
     print("Senti, questo aggeggio serve a calcolare il perimetro di una figura geometrica. Non è scienza missilistica, è solo un'operazione matematica di base. Metti dentro i dati e ti sputa fuori il risultato. Fine.")
     print("""
-   Quadrato -> q
-   Cerchio -> c
-   Rettangolo -> r
+   Quadrato 
+   Cerchio 
+   Rettangolo 
 """)
 
     print("Sì, ok, ma di quale figura vuoi calcolare il perimetro? Quadrato? Triangolo? Cerchio? Dillo e basta...e magari muoviti")
     inpututente = input ("-> ")
 
-    if inpututente == 'q':
+    if inpututente == 'Quadrato':
         print("Ok, allora hai scelto il quadrato. Adesso, dimmi quanto misura un lato. Non ho tutto il giorno...")
         lato = float(input("Dammi il lato, veloce:  "))
         print(f"Il perimetro del Quadrato è: {lato * 4}")
 
-    elif inpututente == 'c':  # Ora questa è la sezione del Cerchio
+    elif inpututente == 'Cerchio':  # Ora questa è la sezione del Cerchio
         print("Ok, allora hai scelto il cerchio. Adesso, dimmi quanto misura un lato. Non ho tutto il giorno...")
         raggio = float(input("Dammi il raggio, veloce:  "))
         # Uso la librea math.pi per una maggiore precisione rispetto a scrivere 3.14
         print(f"Il perimetro del cerchio è: {2 * math.pi * raggio}")
 
-    elif inpututente == 'r':  # Ora questa è la sezione del Rettangolo
+    elif inpututente == 'Rettangolo':  # Ora questa è la sezione del Rettangolo
         print("Ok, allora hai scelto il rettangolo. Adesso, dimmi quanto misura un lato. Non ho tutto il giorno...")
         base = float(input("Daje dammi sta base su:  "))
         altezza = float(input("Daje dammi sta altezza su:  "))
